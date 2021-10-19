cb56dd099a9c8fc9fe27d4028ca08f6772040576
JaoMcs <joaort.azul@gmail.com>
2021-10-19 20:11:03 -0300

    Criação da Navigation que vai lidar com o my task

---
+++ todolistapp/Model/MyTaskTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MyTaskTableViewController.swift
+//  todolistapp
+//
+//  Created by João Marcos on 19/10/21.
+//
+
+import UIKit
+
+class MyTaskTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
